a6bd37ee99eb9a24421430c2ce3d056079eda089
lambo <lambonirinarufin@gmail.com>
2023-02-07 17:15:01 +0300

    toujours token

--- front-end/src/app/dashboard/dashboard.component.ts
+++ front-end/src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { Globalconstants } from '../global-constants';
+import { DashboardService } from '../services/dashboard.service';
+import { SnackbarService } from '../services/snackbar.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements AfterViewInit {
+
+  responseMessage: any;
+  data: any;
+  ngAfterViewInit() { }
+  constructor(private dashboardService: DashboardService,
+    private ngxService: NgxUiLoaderService,
+    private snackbarService: SnackbarService) {
+
+    this.ngxService.start();
+    this.dashboardData();
+  }
+
+  dashboardData() {
+    this.dashboardService.getDetails().subscribe({
+      next: (response: any) => {
+        this.ngxService.stop();
+        this.data = response;
+      }, error: (error) => {
+        this.ngxService.stop();
+        if (error.error?.message) {
+          this.responseMessage = error.error?.message;
+        } else {
+          this.responseMessage = Globalconstants.genericError;
+        }
+        // this.snackbarService.openSnackBar(this.responseMessage,Globalconstants.error);
+      }
+    }
+    )
+  }
 
 }
